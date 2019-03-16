78afc3cd4cdfb85daae15c9a7753bbf7c9a4277f
Owen Dugmore <oadugmore@outlook.com>
2019-03-16 15:34:10 -0700

    update db with video and movie info

--- app/Http/Controllers/Upload/UploadController.php
+++ app/Http/Controllers/Upload/UploadController.php
@@ -6,59 +6,77 @@ use App\Http\Controllers\Controller;
 //use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Input;
 use DB;
 
 class UploadController extends Controller {
 
     function index() {
-        return view('upload');
+        return view('upload', ['status' => 0]);
     }
 
-    function uploadFile(Request $request) 
-    {
-        if (Request::hasFile('video')) 
-        {
+    function uploadFile() {
+        if (Request::hasFile('video')) {
             $file = Request::file('video');
             $filename = $file->getClientOriginalName();
             //$path = public_path() . '/uploads';
-
             //Display File Name
-            echo 'File Name: ' . $file->getClientOriginalName();
-            echo '<br>';
+//            echo 'File Name: ' . $file->getClientOriginalName();
+//            echo '<br>';
 
             //Display File Extension
-            echo 'File Extension: ' . $file->getClientOriginalExtension();
-            echo '<br>';
+//            echo 'File Extension: ' . $file->getClientOriginalExtension();
+//            echo '<br>';
 
             //Display File Real Path
-            echo 'File Real Path: ' . $file->getRealPath();
-            echo '<br>';
+//            echo 'File Real Path: ' . $file->getRealPath();
+//            echo '<br>';
 
             //Display File Size
-            echo 'File Size: ' . $file->getSize();
-            echo '<br>';
+//            echo 'File Size: ' . $file->getSize();
+//            echo '<br>';
 
             //Display File Mime Type
-            echo 'File Mime Type: ' . $file->getMimeType();
-            echo '<br>';
+//            echo 'File Mime Type: ' . $file->getMimeType();
+//            echo '<br>';
 
             //Move Uploaded File
             //$destinationPath = 'uploads';
             //$file->move($destinationPath, $file->getClientOriginalName());
-
             //$file->move($path);
-
+            
             // use laravel Storage facade method to store file,
             // using the private option so the URL cannot be discovered
-            $path = Storage::putFile('videos', $file, 'private');
-            echo 'Server path: ' . $path;
+            $path = Storage::putFileAs('videos', $file, $filename, 'private');
 
-            // enter URL in DB
-            // $title
-        }
-        else
-        {
-            echo 'No file chosen!';
+            // get video metadata
+            $title = Request::get('title');
+            $year = Request::get('year');
+            $summary = Request::get('summary');
+            $sub = Request::get('subscription');
+            $ismovie = Request::get('videotype');
+            $runtime = Request::get('hours') . ':' . Request::get('minutes') . ':' . Request::get('seconds');
+
+            // enter video info in DB
+            DB::table('video')->insertGetId(
+                    ['Title' => $title, 'Year' => $year, 'Summary' => $summary, 'Subscription' => $sub, 'IsMovie' => $ismovie],
+                    'Video_ID');
+            
+            // enter movie info in DB
+            if ($ismovie == 1)
+            {
+                DB::table('movie')->insertGetId(
+                        ['File_Path' => $path, 'Length' => $runtime]);
+            }
+            // enter episode info in DB
+            else
+            {
+                
+            }
+            
+            return view('upload', ['status' => 1]);
+        } else {
+            return view('upload', ['status' => 0]);
         }
     }
 
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -14,25 +14,34 @@
 
         <form id="form" method="POST" enctype="multipart/form-data">
             @csrf
-            <input type="radio" name="videotype" id="videotype" value="movie" onclick="EnableVideo(form)"> Movie
-            <input type="radio" name="videotype" id="videotype" value="episode" > Episode <br/>
-            <input type="text" name="title" id="title" placeholder="Title"> <br/>
-            <input type="number" name="year" id="year" minlength="4" maxlength="4" placeholder="Year" > <br/>
-            <input type="text" name="description" id="description" placeholder="Description" > <br/>
-            <input type="text" name="subscription" id="subscription" placeholder="Subscription" > <br/>
-            <input type="file" name="video" id="video" >
+            <input type="radio" name="videotype" id="videotype" value="1" required> Movie
+            <input type="radio" name="videotype" id="videotype" value="0" > Episode <br>
+            <input type="text" name="title" id="title" placeholder="Title" required> <br>
+            <input type="text" name="year" id="year" minlength="4" maxlength="4" placeholder="Year" required> <br>
+            <input type="text" name="summary" id="summary" placeholder="Summary" required> <br/>
+            <input type="text" name="subscription" id="subscription" placeholder="Subscription" required> <br>
+            <label>Runtime: 
+                <input type="number" name="hours" id="hours" min="0" max="10" required> :
+                <input type="number" name="minutes" id="minutes" min="0" max="59" required> :
+                <input type="number" name="seconds" id="seconds" min="0" max="59" required>
+            </label> <br>
+            <input type="file" name="video" id="video" required>
             <input type="submit" name="submit" >
         </form>
+        
+        <br>
+        
+        @if ($status === 1)
+        <h2>Upload success</h2>
+        @elseif ($status === 2)
+        <h2>Upload failed</h2>
+        @endif
         
         
         @endsection
 
             <?php
-//                function EnableVideo($form form)
-//                {
-//                    $doc = new DOMDocument;
-//                    
-//                }
+                
             ?>
     </body>
 </html>
