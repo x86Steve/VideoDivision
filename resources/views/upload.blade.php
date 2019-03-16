206f186c35ad17866a7e234ef8b1323470151ead
Owen Dugmore <oadugmore@outlook.com>
2019-03-15 17:33:30 -0700

    get more data

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -12,18 +12,27 @@
         
         <h1>Upload video</h1>
 
-        <form method="POST" enctype="multipart/form-data">
+        <form id="form" method="POST" enctype="multipart/form-data">
             @csrf
-            <input type="text" name="title" id="title" />
-            <input type="file" name="video" id="video" />
-            <input type="submit" name="submit" value="Upload" />
+            <input type="radio" name="videotype" id="videotype" value="movie" onclick="EnableVideo(form)"> Movie
+            <input type="radio" name="videotype" id="videotype" value="episode" > Episode <br/>
+            <input type="text" name="title" id="title" placeholder="Title"> <br/>
+            <input type="number" name="year" id="year" minlength="4" maxlength="4" placeholder="Year" > <br/>
+            <input type="text" name="description" id="description" placeholder="Description" > <br/>
+            <input type="text" name="subscription" id="subscription" placeholder="Subscription" > <br/>
+            <input type="file" name="video" id="video" >
+            <input type="submit" name="submit" >
         </form>
         
         
         @endsection
 
             <?php
-            // put your code here
+//                function EnableVideo($form form)
+//                {
+//                    $doc = new DOMDocument;
+//                    
+//                }
             ?>
     </body>
 </html>
