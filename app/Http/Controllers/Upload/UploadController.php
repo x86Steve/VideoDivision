61d709205254e9ce125beaf67ad855aa4d2c8521
Owen Dugmore <oadugmore@outlook.com>
2019-03-17 16:59:46 -0700

    basically works idk

--- app/Http/Controllers/Upload/UploadController.php
+++ app/Http/Controllers/Upload/UploadController.php
@@ -6,78 +6,81 @@ use App\Http\Controllers\Controller;
 //use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Input;
 use DB;
 
 class UploadController extends Controller {
 
     function index() {
-        return view('upload', ['status' => 0]);
+        return view('upload', ['status' => 0, 'newShowId' => -1]);
     }
 
     function uploadFile() {
         if (Request::hasFile('video')) {
             $file = Request::file('video');
             $filename = $file->getClientOriginalName();
+            $path = Storage::putFileAs('videos', $file, $filename, 'private');
+        }
             //$path = public_path() . '/uploads';
             //Display File Name
 //            echo 'File Name: ' . $file->getClientOriginalName();
 //            echo '<br>';
-
             //Display File Extension
 //            echo 'File Extension: ' . $file->getClientOriginalExtension();
 //            echo '<br>';
-
             //Display File Real Path
 //            echo 'File Real Path: ' . $file->getRealPath();
 //            echo '<br>';
-
             //Display File Size
 //            echo 'File Size: ' . $file->getSize();
 //            echo '<br>';
-
             //Display File Mime Type
 //            echo 'File Mime Type: ' . $file->getMimeType();
 //            echo '<br>';
-
             //Move Uploaded File
             //$destinationPath = 'uploads';
             //$file->move($destinationPath, $file->getClientOriginalName());
             //$file->move($path);
-            
             // use laravel Storage facade method to store file,
             // using the private option so the URL cannot be discovered
-            $path = Storage::putFileAs('videos', $file, $filename, 'private');
 
             // get video metadata
             $title = Request::get('title');
             $year = Request::get('year');
             $summary = Request::get('summary');
             $sub = Request::get('subscription');
-            $ismovie = Request::get('videotype');
+            $mediatype = Request::get('mediatype');
             $runtime = Request::get('hours') . ':' . Request::get('minutes') . ':' . Request::get('seconds');
+            $showId = Request::get('showId');
+            $seasonNumber = Request::get('seasonNumber');
+            $episodeNumber = Request::get('episodeNumber');
+            $newShowId = -1;
 
             // enter video info in DB
-            DB::table('video')->insertGetId(
-                    ['Title' => $title, 'Year' => $year, 'Summary' => $summary, 'Subscription' => $sub, 'IsMovie' => $ismovie],
-                    'Video_ID');
-            
+            if ($mediatype != "episode") {
+                DB::table('video')->insertGetId(
+                        ['Title' => $title, 'Year' => $year, 'Summary' => $summary, 'Subscription' => $sub, 'IsMovie' => ($mediatype == "movie") ? 1 : 0]);
+                if ($mediatype == "show") {
+                    $newShowId = DB::table('video')->select('Video_ID')->where('Title', $title)->first();
+                }
+            }
+
             // enter movie info in DB
-            if ($ismovie == 1)
-            {
+            if ($mediatype == "movie") {
                 DB::table('movie')->insertGetId(
                         ['File_Path' => $path, 'Length' => $runtime]);
             }
             // enter episode info in DB
-            else
-            {
-                
+            else if ($mediatype == "episode") {
+                DB::table('episode')->insertGetId(
+                        ['Show_ID' => $showId, 'Season_Number' => $seasonNumber, 'Episode_Number' => $episodeNumber, 
+                            'Episode_Title' => $title, 'File_Path' => $path, 'Length' => $runtime, 'Episode_Summary' => $summary]);
             }
-            
-            return view('upload', ['status' => 1]);
-        } else {
-            return view('upload', ['status' => 0]);
-        }
+
+
+            return view('upload', ['status' => 1, 'newShowId' => $newShowId]);
+//        } else {
+//            return view('upload', ['status' => 0, 'newShowId' => -1]);
+//        }
     }
 
 }
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -9,39 +9,110 @@
         @extends('layouts.app')
 
         @section('content')
-        
+
+        <script>
+            function updateForm()
+            {
+                //var type = document.getElement("mediatype").value;
+                var type = document.querySelector('input[name="mediatype"]:checked').value;
+                //window.alert("type: " + type);
+                //window.alert();
+                //var show = type === "show";
+                //var episode = type === "episode";
+
+                //document.getElementById("episodeFields").style.display = (type === "episode") ? "block" : "none";
+                //document.getElementById("nonShowFields").style.display = (type === "show") ? "none" : "block";
+                //document.getElementById("nonEpisodeFields").style.display = (type === "episode") ? "none" : "block";
+
+                var i;
+                var episodeElements = document.getElementById("episodeFields").querySelectorAll("input");
+                for (i = 0; i < episodeElements.length; i++)
+                {
+                    episodeElements[i].disabled = (type === "episode") ? false : true;
+                }
+
+                var nonShowElements = document.getElementById("nonShowFields").querySelectorAll("input");
+                for (i = 0; i < nonShowElements.length; i++)
+                {
+                    nonShowElements[i].disabled = (type === "show") ? true : false;
+                }
+
+                var nonEpisodeFields = document.getElementById("nonEpisodeFields").querySelectorAll("input");
+                for (i = 0; i < nonEpisodeFields.length; i++)
+                {
+                    nonEpisodeFields[i].disabled = (type === "episode") ? true : false;
+                }
+
+//                if (type === "episode")
+//                {
+//                    document.getElementById("episodeFields").removeAttribute("disabled");
+//                    document.getElementById("nonEpisodeFields").setAttribute("disabled", "disabled");
+//                }
+//                else
+//                {
+//                    document.getElementById("episodeFields").setAttribute("disabled", "disabled");
+//                    document.getElementById("nonEpisodeFields").removeAttribute("disabled");
+//                }
+//                
+//                if (type === "show")
+//                {
+//                    document.getElementById("nonShowFields").setAttribute("disabled", "disabled");
+//                }
+//                else
+//                {
+//                    document.getElementById("nonShowFields").removeAttribute("disabled");
+//                }
+                //document.getElementById("episodeFields").setAttribute("disabled", ) = (type === "episode") ? " : true;
+                //document.getElementById("nonShowFields").disabled = (type === "show") ? true : false;
+
+                document.getElementById("episodeFields").hidden = (type === "episode") ? false : true;
+                document.getElementById("nonShowFields").hidden = (type === "show") ? true : false;
+                document.getElementById("nonEpisodeFields").hidden = (type === "episode") ? true : false;
+            }
+            //function toggle_
+        </script>
+
         <h1>Upload video</h1>
 
         <form id="form" method="POST" enctype="multipart/form-data">
             @csrf
-            <input type="radio" name="videotype" id="videotype" value="1" required> Movie
-            <input type="radio" name="videotype" id="videotype" value="0" > Episode <br>
+            <input type="radio" name="mediatype" id="moviechoice" onclick="updateForm()" checked="true" value="movie" required> Movie
+            <input type="radio" name="mediatype" id="episodechoice" onclick="updateForm()" value="episode" > Episode 
+            <input type="radio" name="mediatype" id="showchoice" onclick="updateForm()" value="show" > Show <br>
             <input type="text" name="title" id="title" placeholder="Title" required> <br>
             <input type="text" name="year" id="year" minlength="4" maxlength="4" placeholder="Year" required> <br>
+            <div id="episodeFields" hidden>
+                <input type="number" name="showId" id="showId" placeholder="Show ID" required disabled="true" > <br>
+                <input type="number" name="seasonNumber" id="seasonNumber" placeholder="Season number" required disabled="true"> <br>
+                <input type="number" name="episodeNumber" id="episodeNumber" placeholder="Episode number" required disabled="true"> <br>
+            </div>
             <input type="text" name="summary" id="summary" placeholder="Summary" required> <br/>
-            <input type="text" name="subscription" id="subscription" placeholder="Subscription" required> <br>
-            <label>Runtime: 
-                <input type="number" name="hours" id="hours" min="0" max="10" required> :
-                <input type="number" name="minutes" id="minutes" min="0" max="59" required> :
-                <input type="number" name="seconds" id="seconds" min="0" max="59" required>
-            </label> <br>
-            <input type="file" name="video" id="video" required>
-            <input type="submit" name="submit" >
+            <div id="nonEpisodeFields">
+                <input type="text" name="subscription" id="subscription" placeholder="Subscription" required> <br>
+            </div>
+            <div id="nonShowFields">
+                <br> <label>Runtime: 
+                    <input type="number" name="hours" id="hours" min="0" max="10" placeholder="h" required> :
+                    <input type="number" name="minutes" id="minutes" min="0" max="59" placeholder="m" required> :
+                    <input type="number" name="seconds" id="seconds" min="0" max="59" placeholder="s" required>
+                </label> <br> <br>
+                <input type="file" name="video" id="video" required>
+            </div>
+            <br> <input type="submit" name="submit" >
         </form>
-        
+
+
         <br>
-        
-        @if ($status === 1)
+
+        @if ($status === 1 && $newShowId == "-1")
         <h2>Upload success</h2>
+        @elseif ($newShowId != "-1")
+        <h2>Created new show with ID {{serialize($newShowId)}}</h2>
         @elseif ($status === 2)
         <h2>Upload failed</h2>
-        @endif
-        
-        
+        @endif       
+
         @endsection
 
-            <?php
-                
-            ?>
     </body>
 </html>
