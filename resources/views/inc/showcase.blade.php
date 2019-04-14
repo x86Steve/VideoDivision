c2d37e7402a002b7b3634a106aa55da4e20e6e4a
Steve <scoseguera@cpp.edu>
2019-04-14 13:23:42 -0700

    - Updated an issue on homepage where it would show login/register always. Changed it so we see a username greeting instead!

--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -1,6 +1,3 @@
-<?php
-
-echo('
 
 <div class="jumbotron jumbotron-fluid" xmlns="http://www.w3.org/1999/html">
 
@@ -16,14 +13,21 @@ echo('
     <h1 class="display-4">Welcome to VideoDivision.net</h1>
     <p class="lead">Say goodbye to Netflix , Hulu, and Crunchyroll!</p>
     <hr class="my-4">
+    @if(Auth::check())
     <p>We can start you off by Logging in or, if you are new, Register now!</p>
+    @endif
+
+    @if(!Auth::check())
     <a class="btn btn-primary btn-lg" href="/public/login" role="button">Login</a>
     <a class="btn btn-primary btn-lg" href="/public/register" role="button">Register</a>
+        @else
+        <a class="btn btn-primary btn-lg" href="/public/profile" role="button">Welcome, {{Auth::user()->username}}</a>
+        @endif
 </center>
 
   </div>
   <!-- /.container -->
 </div>
 <!-- /.jumbotron -->
-');
-?>
+
+
