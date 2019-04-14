2279ac43fc9c85a2a644d8617f3e2a50afb231fe
Steve <scoseguera@cpp.edu>
2019-04-14 04:16:16 -0700

    - Added a new neat homepage ! :)

--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -2,13 +2,28 @@
 
 echo('
 
-<div class="jumbotron text-center">
-    <div class="container" >
-        <h1> Welcome to VideoDivision.net</h1>
-        <p class="lead">
-            Say goodbye to Netflix , Hulu, and Crunchyroll!
-        </p>
-    </div>
+<div class="jumbotron jumbotron-fluid" xmlns="http://www.w3.org/1999/html">
+
+<video autoplay muted loop poster="">
+    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4">
+    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.webm" type="video/webm">
+    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.ogv" type="video/ogg">
+</video>
+
+  <div class="container text-white">
+
+<center>
+    <h1 class="display-4">Welcome to VideoDivision.net</h1>
+    <p class="lead">Say goodbye to Netflix , Hulu, and Crunchyroll!</p>
+    <hr class="my-4">
+    <p>We can start you off by Logging in or, if you are new, Register now!</p>
+    <a class="btn btn-primary btn-lg" href="/public/login" role="button">Login</a>
+    <a class="btn btn-primary btn-lg" href="/public/register" role="button">Register</a>
+</center>
+
+  </div>
+  <!-- /.container -->
 </div>
+<!-- /.jumbotron -->
 ');
 ?>
