2515149516fb6913e4a9166a2720b017a88e16b7
Steve <scoseguera@cpp.edu>
2019-04-23 02:07:02 -0700

    - Fixed watch video and video details in accordance to the removal of html from layout.app

--- resources/views/view_video_details.blade.php
+++ resources/views/view_video_details.blade.php
@@ -1,182 +1,200 @@
 @extends('layouts.app')
 
 @section('content')
-    {{--WARNING, THIS CODE IS CANCER--}}
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-11 order-lg-1">
+        {{--WARNING, THIS CODE IS CANCER--}}
 
-    {{--Display video thumbnail on page--}}
-    <br>
-    <br>
-    <img src="http://videodivision.net/assets/images/thumbnails/<?php echo ($file)[0]->Video_ID?>.jpg" width="240"
-         height="360"
-         alt="Place Holder" title="Production Title"
-         align="left">
-
-    {{--BASIC INFO (TITLE, RATING, YEAR, SUBSCRIPTION--}}
-    <h2>
+        {{--Display video thumbnail on page--}}
         <br>
-        &nbsp;&nbsp; <strong>Title:</strong> <?php echo ($file)[0]->Title?>
-    </h2>
-    <h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <strong>Rating: </strong> <?php echo ($file)[0]->Current_Rating ?></h5>
-    <h2>
-        &nbsp;&nbsp; <strong>Year:</strong> <?php echo ($file)[0]->Year?> <br> <br>
-        &nbsp;&nbsp; <strong>Subscription:</strong> <?php echo ($file)[0]->Subscription?> <br> <br></h2>
+        <br>
+        <img src="http://videodivision.net/assets/images/thumbnails/<?php echo ($file)[0]->Video_ID?>.jpg" width="240"
+             height="360"
+             alt="Place Holder" title="Production Title"
+             align="left">
 
-    {{--IF MOVIE DISPLAY LENGTH, IF EPISODE DISPLAY AMOUNT OF EPISODES--}}
-    @if ($isMovie === 1)
+        {{--BASIC INFO (TITLE, RATING, YEAR, SUBSCRIPTION--}}
+        <h2>
+            <br>
+            &nbsp;&nbsp; <strong>Title:</strong> <?php echo ($file)[0]->Title?>
+        </h2>
+        <h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <strong>Rating: </strong> <?php echo ($file)[0]->Current_Rating ?></h5>
+        <h2>
+            &nbsp;&nbsp; <strong>Year:</strong> <?php echo ($file)[0]->Year?> <br> <br>
+            &nbsp;&nbsp; <strong>Subscription:</strong> <?php echo ($file)[0]->Subscription?> <br> <br></h2>
 
-        <h3><strong>&nbsp;&nbsp;&nbsp;&nbsp;Length: </strong><?php echo $extra->Length?> </h3>
+        {{--IF MOVIE DISPLAY LENGTH, IF EPISODE DISPLAY AMOUNT OF EPISODES--}}
+        @if ($isMovie === 1)
 
-    @else
-        <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;Number of Seasons:</strong> <?php echo $extra->Season_Number?></h4>
-        <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;Number of Episodes:</strong> <?php echo $num_of_episodes?></h4>
-    @endif
-    <br>
+            <h3><strong>&nbsp;&nbsp;&nbsp;&nbsp;Length: </strong><?php echo $extra->Length?> </h3>
 
-    {{--A BUNCH OF SPACES FOR FORMATTING (Screw using align)--}}
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-    {{--IF THE USER IS SUBBED GIVE ACCESS TO WATCH NOW BUTTON--}}
-    @if($isSubbed === true)
-        @if ($isMovie == 1)
-
-            <a href="/public/watch/<?php echo ($file)[0]->Video_ID?>">
-                <button type="submit" class="btn btn-dark">Watch Now!</button>
-            </a>
         @else
-            <a href="/public/view/<?php echo ($file)[0]->Video_ID?>">
-                <button type="submit" class="btn btn-dark">Watch Now!</button>
-            </a>
+            <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;Number of Seasons:</strong> <?php echo $extra->Season_Number?></h4>
+            <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;Number of Episodes:</strong> <?php echo $num_of_episodes?></h4>
         @endif
+        <br>
 
-        {{--OTHERWISE GIVE ACCESS TO SUBSCRIBE BUTTON--}}
-    @else
-        <button
-                type="button"
-                class="btn btn-dark"
-                data-toggle="modal"
-                data-target="#subscribeModal">
-            Subscribe!
-        </button>
-        {{--IF USER IS LOGGED IN LET THEM SUBSCRIBE--}}
-        @if($User_ID != -1)
-            <div class="modal fade" id="subscribeModal"
-                 tabindex="-1" role="dialog"
-                 aria-labelledby="subscribeModalLabel">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title"
-                                id="subscribeModalLabel">Subscribe?</h4>
-                        </div>
-                        <div class="modal-body">
-                            <p>
-                                Please confirm you would like to subscribe to
-                                <b><span id="sub-title"><?php echo ($file)[0]->Title?></span></b>
-                                .
-                            </p>
+        {{--A BUNCH OF SPACES FOR FORMATTING (Screw using align)--}}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        {{--IF THE USER IS SUBBED GIVE ACCESS TO WATCH NOW BUTTON--}}
+        @if($isSubbed === true)
+            @if ($isMovie == 1)
+
+                <a href="/public/watch/<?php echo ($file)[0]->Video_ID?>">
+                    <button type="submit" class="btn btn-dark">Watch Now!</button>
+                </a>
+            @else
+                <a href="/public/view/<?php echo ($file)[0]->Video_ID?>">
+                    <button type="submit" class="btn btn-dark">Watch Now!</button>
+                </a>
+            @endif
+
+            {{--OTHERWISE GIVE ACCESS TO SUBSCRIBE BUTTON--}}
+        @else
+            <button
+                    type="button"
+                    class="btn btn-dark"
+                    data-toggle="modal"
+                    data-target="#subscribeModal">
+                Subscribe!
+            </button>
+            {{--IF USER IS LOGGED IN LET THEM SUBSCRIBE--}}
+            @if($User_ID != -1)
+                <div class="modal fade" id="subscribeModal"
+                     tabindex="-1" role="dialog"
+                     aria-labelledby="subscribeModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title"
+                                    id="subscribeModalLabel">Subscribe?</h4>
                             </div>
-                        {{--IF USER IS NOT LOGGED IN ASK THEM TO LOGIN TO SUBSCRIBE--}}
-                    @else
-                        <div class="modal fade" id="subscribeModal"
-                             tabindex="-1" role="dialog"
-                             aria-labelledby="subscribeModalLabel">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title"
-                                            id="subscribeModalLabel">Please Login</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>
-                                            Please Login to subscribe to this video.
-                                        </p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button"
-                                                class="btn btn-dark"
-                                                data-dismiss="modal">Cancel
-                                        </button>
-                                        <span class="pull-right">
-
-                    <a class="btn btn-dark" href="/public/login">Login</a>
-            </span>
-                                    </div>
-                                </div>
+                            <div class="modal-body">
+                                <p>
+                                    Please confirm you would like to subscribe to
+                                    <b><span id="sub-title"><?php echo ($file)[0]->Title?></span></b>
+                                    .
+                                </p>
                             </div>
-                        </div>
-                    @endif
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-dark" data-dismiss="modal">Cancel</button>
+                                <button type="submit" form="form" class="btn btn-dark">Subscribe</button>
+                                <form id="form" method="post">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" id="User_ID" name="User_ID" value="<?php echo $User_ID?>">
+                                    <input type="hidden" id="Video_ID" name="Video_ID" value="<?php echo ($file)[0]->Video_ID?>">
+                                    <input type="hidden" id="isMovie" name="isMovie" value="<?php echo $isMovie?>">
+                                    <input type="hidden" id="postType" name="postType" value="0">
+                                </form>
 
-                @endif
-
-                {{--A BUNCH OF SPACES FOR FORMATTING (Screw using align)--}}
-
-
-
-                {{--THE FAVORITE BUTTON--}}
-
-                @if($isFav === false)
-                    <form id="form" method="post">
-                        {{ csrf_field() }}
-                        <input type="hidden" id="User_ID" name="User_ID" value="<?php echo $User_ID?>">
-                        <input type="hidden" id="Video_ID" name="Video_ID" value="<?php echo ($file)[0]->Video_ID?>">
-                        <input type="hidden" id="isMovie" name="isMovie" value="<?php echo $isMovie?>">
-                        <input type="hidden" id="postType" name="postType" value="1">
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button type="submit" class="btn btn-dark btn-sm">Favorite!</button>
-                    </form>
-                @else
-                    <form id="form" method="post">
-                        {{ csrf_field() }}
-                        <input type="hidden" id="User_ID" name="User_ID" value="<?php echo $User_ID?>">
-                        <input type="hidden" id="Video_ID" name="Video_ID" value="<?php echo ($file)[0]->Video_ID?>">
-                        <input type="hidden" id="isMovie" name="isMovie" value="<?php echo $isMovie?>">
-                        <input type="hidden" id="postType" name="postType" value="2">
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button type="submit" class="btn btn-dark btn-sm">Un-favorite</button>
-                    </form>
-                @endif
-
-
-                {{--OTHER BASIC INFO (SUMMARY, GENRES, CAST, DIRECTORS)--}}
-                <br>
-                <br>
-                <br>
-                <h3><strong>Summary:</strong> <?php echo ($file)[0]->Summary?> <br> <br></h3>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm">
-
-                            <h3><strong>Genre(s):</strong></h3>
-                            @foreach($genres as $genre)
-                                <h4>{{$genre['Name']}}&nbsp;&nbsp;</h4>
-                            @endforeach
-                        </div>
-                        <div class="col-sm">
-                            <h3><strong>Actor(s):</strong></h3>
-                            @foreach($cast as $actor)
-                                <h4> {{$actor['First_Name']}} {{$actor['Last_Name']}}</h4>
-                            @endforeach
-                        </div>
-                        <div class="col-sm">
-                            <h3><strong>Director(s):</strong></h3>
-                            @foreach($directors as $director)
-                                <h4> {{$director['First_Name']}} {{$director['Last_Name']}} </h4>
-                            @endforeach
-
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {{--IF USER IS NOT LOGGED IN ASK THEM TO LOGIN TO SUBSCRIBE--}}
+            @else
+                <div class="modal fade" id="subscribeModal"
+                     tabindex="-1" role="dialog"
+                     aria-labelledby="subscribeModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title"
+                                    id="subscribeModalLabel">Please Login</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p>
+                                    Please Login to subscribe to this video.
+                                </p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button"
+                                        class="btn btn-dark"
+                                        data-dismiss="modal">Cancel
+                                </button>
+                                <span class="pull-right">
+
+                        <a class="btn btn-dark" href="/public/login">Login</a>
+                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+        @endif
+
+        {{--A BUNCH OF SPACES FOR FORMATTING (Screw using align)--}}
 
 
+
+        {{--THE FAVORITE BUTTON--}}
+
+        @if($isFav === false)
+            <form id="form" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" id="User_ID" name="User_ID" value="<?php echo $User_ID?>">
+                <input type="hidden" id="Video_ID" name="Video_ID" value="<?php echo ($file)[0]->Video_ID?>">
+                <input type="hidden" id="isMovie" name="isMovie" value="<?php echo $isMovie?>">
+                <input type="hidden" id="postType" name="postType" value="1">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button type="submit" class="btn btn-dark btn-sm">Favorite!</button>
+            </form>
+        @else
+            <form id="form" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" id="User_ID" name="User_ID" value="<?php echo $User_ID?>">
+                <input type="hidden" id="Video_ID" name="Video_ID" value="<?php echo ($file)[0]->Video_ID?>">
+                <input type="hidden" id="isMovie" name="isMovie" value="<?php echo $isMovie?>">
+                <input type="hidden" id="postType" name="postType" value="2">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button type="submit" class="btn btn-dark btn-sm">Un-favorite</button>
+            </form>
+        @endif
+
+
+        {{--OTHER BASIC INFO (SUMMARY, GENRES, CAST, DIRECTORS)--}}
+        <br>
+        <br>
+        <br>
+        <h3><strong>Summary:</strong> <?php echo ($file)[0]->Summary?> <br> <br></h3>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+
+                    <h3><strong>Genre(s):</strong></h3>
+                    @foreach($genres as $genre)
+                        <h4>{{$genre['Name']}}&nbsp;&nbsp;</h4>
+                    @endforeach
+                </div>
+                <div class="col-sm">
+                    <h3><strong>Actor(s):</strong></h3>
+                    @foreach($cast as $actor)
+                        <h4> {{$actor['First_Name']}} {{$actor['Last_Name']}}</h4>
+                    @endforeach
+                </div>
+                <div class="col-sm">
+                    <h3><strong>Director(s):</strong></h3>
+                    @foreach($directors as $director)
+                        <h4> {{$director['First_Name']}} {{$director['Last_Name']}} </h4>
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
             </div>
         </div>
     </div>
-@endsection
 
+
+@endsection
 
 
 
--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -1,67 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 style="font-size:60px;"><span class="bigger"> <?php echo ($file)[0]->Title?></span></h1>
-    <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
-    
-                @if ($isMovie == 0)
-                    <p>
-                        <strong>S<?php echo $season_number?>: E<?php echo $episode_number?> </strong>
-                        <?php echo $episode_title?>
-                    </p>
-                @endif
-                <body>
-                <video id="player" autoplay class="video-js" width='768' height='432'
-                       controls
-                       poster="http://videodivision.net/assets/images/thumbnails/<?php echo ($file)[0]->Video_ID?>.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-11 order-lg-1">
+            <h1 style="font-size:60px;"><span class="bigger"> <?php echo ($file)[0]->Title?></span></h1>
+            <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 
-                    <source src="http://videodivision.net{{($file_path)}}" type="video/mp4"/>
-                    <p class='vjs-no-js'>
-                        To view this video please enable JavaScript, and consider upgrading to a web browser that
-                        <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
-                    </p>
-                </video>
-    <script>
-        var myPlayer = videojs('player');
-        videojs("player", {}, function(){
+                        @if ($isMovie == 0)
+                            <p>
+                                <strong>S<?php echo $season_number?>: E<?php echo $episode_number?> </strong>
+                                <?php echo $episode_title?>
+                            </p>
+                        @endif
+                        <body>
+                        <video id="player" autoplay class="video-js" width='768' height='432'
+                               controls
+                               poster="http://videodivision.net/assets/images/thumbnails/<?php echo ($file)[0]->Video_ID?>.jpg">
 
-        });
-    </script>
+                            <source src="http://videodivision.net{{($file_path)}}" type="video/mp4"/>
+                            <p class='vjs-no-js'>
+                                To view this video please enable JavaScript, and consider upgrading to a web browser that
+                                <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+                            </p>
+                        </video>
+            <script>
+                var myPlayer = videojs('player');
+                videojs("player", {}, function(){
 
-    {{ActivityEntry("Started watching: " . helper_GetMovieTitleByID(($file)[0]->Video_ID))}}
+                });
+            </script>
 
-    </body>
+            {{ActivityEntry("Started watching: " . helper_GetMovieTitleByID(($file)[0]->Video_ID))}}
 
-    @if ($isMovie == 0)
-        <p>
-        <div class="btn-group" role="group">
-            @if ($is_special_episode == 3)
-                <a class="nav-link"
-                   href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number - 1)}}/episode/{{($previous_episode)}}">
-                    <button type="submit" class="btn btn-dark">Previous Episode</button>
-                </a>
-            @elseif ($is_special_episode == 0 || $is_special_episode == 1 || $is_special_episode == 2)
-                <a class="nav-link"
-                   href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number - 1)}}">
-                    <button type="submit" class="btn btn-dark">Previous Episode</button>
-                </a>
+            </body>
+
+            @if ($isMovie == 0)
+                <p>
+                <div class="btn-group" role="group">
+                    @if ($is_special_episode == 3)
+                        <a class="nav-link"
+                           href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number - 1)}}/episode/{{($previous_episode)}}">
+                            <button type="submit" class="btn btn-dark">Previous Episode</button>
+                        </a>
+                    @elseif ($is_special_episode == 0 || $is_special_episode == 1 || $is_special_episode == 2)
+                        <a class="nav-link"
+                           href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number - 1)}}">
+                            <button type="submit" class="btn btn-dark">Previous Episode</button>
+                        </a>
+                    @endif
+                    @if ($is_special_episode == 0 || $is_special_episode == 3)
+                        <a class="nav-link"
+                           href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number + 1)}}">
+                            <button type="submit" class="btn btn-dark">Next Episode</button>
+                        </a>
+                    @elseif ($is_special_episode == 1)
+                        <a class="nav-link"
+                           href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number + 1)}}/episode/{{(1)}}">
+                            <button type="submit" class="btn btn-dark">Next Episode</button>
+                        </a>
+                    @elseif ($is_special_episode == 4)
+                        <a class="nav-link"
+                           href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number + 1)}}">
+                            <button type="submit" class="btn btn-dark">Next Episode</button>
+                        </a>
+                </div>
             @endif
-            @if ($is_special_episode == 0 || $is_special_episode == 3)
-                <a class="nav-link"
-                   href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number + 1)}}">
-                    <button type="submit" class="btn btn-dark">Next Episode</button>
-                </a>
-            @elseif ($is_special_episode == 1)
-                <a class="nav-link"
-                   href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number + 1)}}/episode/{{(1)}}">
-                    <button type="submit" class="btn btn-dark">Next Episode</button>
-                </a>
-            @elseif ($is_special_episode == 4)
-                <a class="nav-link"
-                   href="/public/watch/{{(($file)[0]->Video_ID)}}/season/{{($season_number)}}/episode/{{($episode_number + 1)}}">
-                    <button type="submit" class="btn btn-dark">Next Episode</button>
-                </a>
-            @endif
+
+        </div>
+        @endif
+        </div>
         </div>
     </div>
 @endsection
