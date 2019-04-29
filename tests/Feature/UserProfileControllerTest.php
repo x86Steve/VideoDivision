85dc001b89d0c91924ad66cc34ce7c45bf332080
Owen Dugmore <oadugmore@outlook.com>
2019-04-29 01:39:16 -0700

    implemented testUpdateProfile

--- tests/Feature/UserProfileControllerTest.php
+++ tests/Feature/UserProfileControllerTest.php
@@ -3,40 +3,67 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Http\Request;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserProfileControllerTest extends TestCase
 {
 
+    // Laravel provides functionality to automatically reset the database after testing
+    // but i can't be assed to learn how migrations work so i'm leaving this commented out for now
+    // uncomment at own risk
+
+    //use RefreshDatabase;
+
     public function testUpdateProfile()
     {
-        Request $testRequest = Request::create('profile/edit', 'POST', 
-        [
-            'firstname' => 'TestFirstName',
-            'lastname' => 'TestLastName',
-            'jobtitle' => 'TestJobTitle',
-            'description' => 'TestDescription',
-            'street' => 'TestStreet',
-            'city' => 'TestCity',
-            'state' => 'TestState',
-            'currentpassword' => 'TestCurrentPassword'
-        ]
-    );
+        // create a test user
+        $testUsername = 'user' . str_random(4);
+        $testUser = factory(User::class)->create(
+            [
+                'password' => Hash::make('TestCurrentPassword'),
+                'lastname' => 'User',
+                'username' => $testUsername
+            ]
+        );
 
-    // assertViewHas? assertSessionHas?
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testResponse = $this->from('profile')->post(
+            'profile/edit',
+            [
+                'firstname' => 'FirstNameEdited',
+                'lastname' => 'LastNameEdited',
+                'jobtitle' => 'JobTitleEdited',
+                'description' => 'DescriptionEdited',
+                'street' => 'StreetEdited',
+                'city' => 'CityEdited',
+                'state' => 'CA',
+                'currentpassword' => 'TestCurrentPassword'
+            ]
+        );
+
+        // should be redirected back to profile/edit
+        $testResponse->assertRedirect('profile/edit');
+
+        // response should have no errors
+        $testResponse->assertSessionHasNoErrors();
+
+        // check if database has updated user information
+        $this->assertDatabaseHas(
+            'users',
+            [
+                'username' => $testUsername,
+                'name' => 'FirstNameEdited',
+                'lastname' => 'LastNameEdited',
+                'jobtitle' => 'JobTitleEdited',
+                'description' => 'DescriptionEdited',
+                'street' => 'StreetEdited',
+                'city' => 'CityEdited',
+                'state' => 'CA'
+            ]);
     }
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+    
 }
