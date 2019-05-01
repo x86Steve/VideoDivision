77af2135e4a78f6dc4b59430ccc8a331f794e17f
Steve <scoseguera@cpp.edu>
2019-04-30 23:24:36 -0700

    - Fixed front page video from not loading properly. - Edited some of the front page's font to be more clean.

--- resources/views/inc/showcase.blade.php
+++ resources/views/inc/showcase.blade.php
@@ -1,30 +1,24 @@
 
 <div class="jumbotron jumbotron-fluid" xmlns="http://www.w3.org/1999/html">
+    <video autoplay muted loop poster="http://why.soserio.us/Media/chrome11556691513.png">
+        <source src="http://why.soserio.us/Media/Movie%20intros%20compilation.mp4" type="video/mp4">
+    </video>
 
-<video autoplay muted loop poster="">
-    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4">
-    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.webm" type="video/webm">
-    <source src="" data-src="//clips.vorwaerts-gmbh.de/big_buck_bunny.ogv" type="video/ogg">
-</video>
-
-  <div class="container text-white">
-
-<center>
-    <h1 class="display-4">Welcome to VideoDivision.net</h1>
-    <p class="lead">Say goodbye to Netflix , Hulu, and Crunchyroll!</p>
-    <hr class="my-4">
-    @if(!Auth::check())
-        <p>We can start you off by Logging in or, if you are new, Register now!</p>
-    <a class="btn btn-primary btn-lg" href="/public/login" role="button">Login</a>
-    <a class="btn btn-primary btn-lg" href="/public/register" role="button">Register</a>
-    @else
-    <a class="btn btn-primary btn-lg" href="/public/profile" role="button">Welcome, {{Auth::user()->username}}</a>
-    @endif
-</center>
-
-  </div>
-  <!-- /.container -->
+    <div class="container text-white">
+        <div class="center">
+            <h1 class="display-5">Welcome to VideoDivision.net</h1>
+            <p class="lead">Say goodbye to Netflix , Hulu, and Crunchyroll!</p>
+            <hr class="my-4">
+            @if(!Auth::check())
+            <p>We can start you off by Logging in or, if you are new, Register now!</p>
+            <a class="btn btn-primary btn-lg" href="/public/login" role="button">Login</a>
+            <a class="btn btn-primary btn-lg" href="/public/register" role="button">Register</a>
+            @else
+            <a class="btn btn-primary btn-lg" href="/public/profile" role="button">Welcome, {{Auth::user()->username}}</a>
+            @endif
+        </div>
+    </div>
 </div>
-<!-- /.jumbotron -->
+
 
 
