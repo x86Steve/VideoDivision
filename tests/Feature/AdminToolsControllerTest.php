b4b0240c777ee913889b5ee58c598ce5dbb99947
Owen Dugmore <oadugmore@outlook.com>
2019-05-17 00:22:08 -0700

    AdminTools and Upload tests

---
+++ tests/Feature/AdminToolsControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Auth;
+use DB;
+
+class UserProfileControllerTest extends TestCase
+{
+    public function testViewPageAsAdmin()
+    {
+        // create a test admin
+        $testUser = $this->CreateTestAdmin();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testRequest = $this->get('admin');
+
+        $testRequest->assertViewIs('admin');
+    }
+
+    public function testFailToViewPageAsNonAdmin()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testRequest = $this->get('admin');
+        $testRequest->assertRedirect('home');
+    }
+
+}
---
+++ tests/Feature/UploadControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Auth;
+use DB;
+
+class UserProfileControllerTest extends TestCase
+{
+
+    public function testUploadMovie()
+    {
+        // create a test admin
+        $testUser = $this->CreateTestAdmin();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        // create test files
+        $testVideoFile = UploadedFile::fake()->create('video.mp4');
+        $testThumbnailFile = UploadedFile::fake()->image('thumb.jpg');
+
+        $testResponse = $this->post('admin/upload',
+        [
+            'title' => 'TestTitle',
+            'year' => 2000,
+            'summary' => 'Test Summary',
+            'subscription' => 'VideoDivision',
+            'mediatype' => 'movie',
+            'duration' => '0:0:0',
+            'video' => $testVideoFile,
+            'thumbnail' => $testThumbnailFile,
+            'actorSelect1' => 1,
+            'directorSelect1' => 1,
+            'genreSelect1' => 1
+        ]);
+
+        // should return the upload view
+        $testResponse->assertViewIs('upload');
+
+        // check the view has status indicating success
+        $testResponse->assertViewHas('status', 1);
+    }
+
+    public function testViewPageAsAdmin()
+    {
+        // create a test admin
+        $testUser = $this->CreateTestAdmin();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testResponse = $this->get('admin/upload');
+
+        $testResponse->assertViewIs('upload');
+    }
+
+    public function testFailToViewPageAsNonAdmin()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testResponse = $this->get('admin/upload');
+        $testResponse->assertRedirect('home');
+    }
+
+    public function testFailToUploadVideoAsNonAdmin()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // Become the user
+        $this->actingAs($testUser);
+
+        $testResponse = $this->post('admin/upload');
+        $testResponse->assertRedirect('home');
+    }
+
+}
