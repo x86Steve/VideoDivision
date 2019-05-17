5a32fd1355fc8d017f3aef822e65a85a31809a89
Owen Dugmore <oadugmore@outlook.com>
2019-05-17 16:16:34 -0700

    view video test

---
+++ tests/Feature/ViewVideoTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Auth;
+use DB;
+
+class ViewVideoTest extends TestCase
+{
+
+    public function testViewVideoDetails()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // become the user
+        $this->actingAs($testUser);
+
+        // get details page for video id 1
+        $testResponse = $this->get('video_details?video=1');
+
+        // test that the correct view was returned
+        $testResponse->assertViewIs('view_video_details');
+
+        // test that the view contains important information
+        $testResponse->assertViewHasAll(
+            [
+                'User_ID' => Auth::user()->id,
+                'isPaid' => false,
+                'isSubbed' => false,
+                'isMovie' => true,
+                'isFav' => false,
+                'directors',
+                'cast',
+                'genres'
+            ]
+        );
+    }
+
+    public function testFavoriteMovie()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // become the user
+        $this->actingAs($testUser);
+
+        //favorite video id 1
+        $testResponse = $this->post(
+            'video_details',
+            [
+                'postType' => 1,
+                'isMovie' => 1,
+                'User_ID' => Auth::user()->id,
+                'Video_ID' => 1
+            ]
+        );
+
+        // test that the correct view was returned
+        $testResponse->assertRedirect('video_details');
+        //$testResponse->assertViewIs('view_video_details');
+    }
+
+    public function testUnfavoriteMovie()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // become the user
+        $this->actingAs($testUser);
+
+        //favorite video id 2
+        $testResponse = $this->post(
+            'video_details',
+            [
+                'postType' => 2,
+                'isMovie' => 1,
+                'User_ID' => Auth::user()->id,
+                'Video_ID' => 1
+            ]
+        );
+
+        // test that the correct view was returned
+        $testResponse->assertRedirect('video_details');
+        //$testResponse->assertViewIs('view_video_details');
+    }
+
+    public function testGetMyVideosView()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // become the user
+        $this->actingAs($testUser);
+
+        // subscribe to a movie
+        $this->testSubscribeToMovie();
+
+        // get
+        $testResponse = $this->get('my_videos');
+
+        // test that the correct view was returned
+        $testResponse->assertViewIs('subscribed_videos');
+
+        $testResponse->assertViewHas('output');
+    }
+
+    public function testSubscribeToMovie()
+    {
+        // create a test user
+        $testUser = $this->CreateTestUser();
+
+        // become the user
+        $this->actingAs($testUser);
+
+        //subscribe user to video id 2
+        $testResponse = $this->post(
+            'video_details',
+            [
+                'postType' => 0,
+                'isMovie' => 1,
+                'User_ID' => Auth::user()->id,
+                'Video_ID' => 1
+            ]
+        );
+
+        // test that the correct view was returned
+        //$testResponse->assertViewIs('view_video_details');
+        $testResponse->assertRedirect('video_details');
+    }
+
+}
