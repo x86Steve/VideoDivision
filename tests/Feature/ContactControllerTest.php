4fac4bedcbce4b090c725ff6d21f2154b1541fbe
Owen Dugmore <oadugmore@outlook.com>
2019-05-16 23:09:57 -0700

    ContactControllerTest

---
+++ tests/Feature/ContactControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Auth;
+use DB;
+
+class UserProfileControllerTest extends TestCase
+{
+
+    public function testViewPage()
+    {
+        $testRequest = $this->get('contact');
+        $testRequest->assertViewIs('contact');
+    }
+
+    public function testSendMessage()
+    {
+        $testRequest = $this->post(
+            'contact',
+            [
+                'name' => "Test User",
+                'email' => 'test@example.com',
+                'message' => "This is a test message sent by a unit test."
+            ]
+        );
+
+        $testRequest->assertRedirect('/');
+    }
+}
