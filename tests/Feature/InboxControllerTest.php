565f9a5b29c69f50082d4eedc2e61112e4f8f02e
Owen Dugmore <oadugmore@outlook.com>
2019-05-17 16:51:37 -0700

    add test message

--- tests/Feature/InboxControllerTest.php
+++ tests/Feature/InboxControllerTest.php
@@ -19,6 +19,17 @@ class InboxControllerTest extends TestCase
         // become the user
         $this->actingAs($testUser);
 
+        // add a test message to the user's profile
+        DB::table('chat_log')->insert(
+            [
+                'Sender_ID' => $testUser->id + 1,
+                'Receiver_ID' => $testUser->id,
+                'Message' => 'Test Message',
+                'Time_Sent' => now(),
+                'isRead' => 0
+            ]
+        );
+
         // going to inbox page
         $testResponse = $this->get('inbox');
         $testResponse->assertViewIs('inbox');
